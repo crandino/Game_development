c6acef460396e58dfc60b572f931298380ebe367
crandino <carlosmanbe@hotmail.com>
2015-09-17 18:41:03 +0200

    DList and DynArray with UnitTest
    Now, it's time to implement append methods mixing DLists and
    DynArrays... :sake:

--- DList.h
+++ DList.h
@@ -1,14 +1,16 @@
-//=================================
-// include guard
 #ifndef __DLIST_H__
 #define __DLIST_H__
-//=================================
-// forward declared dependencies
-//=================================
-// included dependencies
-#include <stdio.h>
-//=================================
-// the actual class
+
+#include <assert.h>
+
+template <class TYPE>
+void swap(TYPE &a, TYPE &b) {
+
+	TYPE tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
+}
 
 template <class TYPE>
 struct doubleNode {
@@ -46,6 +48,27 @@ public:
 	~DList()
 	{
 		clear();
+	}
+
+	/* If this method won't be constant, we will be able to do something like:
+	(b += c).add("hola");
+
+	If this method is const, so:
+	b += c;    --> Here is the important step, the object returned.
+	b.add("hola");
+	*/
+	 
+	const DList<TYPE>& operator+= (DList &list_to_add)
+	{
+		doubleNode<TYPE> *item_to_add = list_to_add.getFirst();
+
+		while (item_to_add != NULL)
+		{
+			add(item_to_add->data);
+			item_to_add = item_to_add->next;
+		}
+
+		return (*this);		
 	}
 
 	unsigned int count() const
@@ -115,7 +138,7 @@ public:
 
 	bool del(doubleNode<TYPE> *node_to_delete)
 	{
-		if (start != NULL)
+		if (start != NULL && node_to_delete != NULL)
 		{
 			if (node_to_delete->previous != NULL)
 			{
@@ -145,7 +168,6 @@ public:
 
 	void clear()
 	{
-
 		doubleNode<TYPE> *item = start;
 		doubleNode<TYPE> *item_next;
 
@@ -159,6 +181,113 @@ public:
 		start = end = NULL;
 		size = 0;
 	
+	}
+
+	bool isOnList(const doubleNode<TYPE> *node_to_check) const
+	{
+		if (start != NULL && node_to_check != NULL)
+		{
+			doubleNode<TYPE> *item = start;
+			while (item != NULL)
+			{
+				if (item->data == node_to_check->data)
+					return true;
+				item = item->next;
+			}
+		}
+		return false;
+	}
+
+	bool insert(const DList &inserted_list, unsigned int position)
+	{
+		if (position < count())
+		{
+			DList<TYPE> tmp_list;
+			for (unsigned int i = position; i < count(); i++)
+			{
+				tmp_list.add(getNodeAtPos(i)->data);
+				del(getNodeAtPos(i));
+			}
+
+			for (unsigned int i = 0; i < inserted_list.count(); i++)
+				add(inserted_list.getNodeAtPos(i)->data);
+
+			for (unsigned int i = 0; i < tmp_list.count(); i++)
+				add(tmp_list.getNodeAtPos(i)->data);
+
+			return true;
+		}
+		return false;
+	}
+
+	unsigned int sort_copy()
+	{
+		unsigned int counter = 0;
+		for (unsigned int i = 0; i < size - 1; i++)
+		{
+			for (unsigned int j = i + 1; j < size; j++)
+			{
+				counter++;
+				if (getNodeAtPos(i)->data > getNodeAtPos(j)->data)
+					swap(getNodeAtPos(i)->data, getNodeAtPos(j)->data);
+			}
+		}
+		return counter;
+	}
+
+	unsigned int sort_reference()
+	{
+		unsigned int counter = 0;
+		doubleNode<TYPE> *first_node;
+		doubleNode<TYPE> *second_node;
+
+		for (unsigned int i = 0; i < size - 1; i++)
+		{
+			first_node = getNodeAtPos(i);
+			for (unsigned int j = i + 1; j < size; j++)
+			{
+				second_node = getNodeAtPos(j);
+				counter++;
+				if (first_node->data > second_node->data)
+				{
+					// millor fer un SWAP propi per la classe DList.
+
+					if (first_node->previous = NULL)
+					{
+						first_node->previous = second_node->previous;
+						second_node->previous = NULL;
+					}
+					else
+						swap(first_node->previous->next, second_node->previous->next);
+
+					if (second_node->next = NULL)
+					{
+						second_node->next = first_node->next;
+						first_node->next = NULL;
+					}
+					else
+						swap(first_node->next->previous, second_node->next->previous);
+
+					swap(first_node->next, second_node->next);
+					swap(first_node->previous, second_node->previous);
+
+				}
+			}
+		}
+		return counter;
+	}
+
+
+	TYPE& operator[] (unsigned int index)
+	{
+		assert(index < size);
+		return getNodeAtPos(index)->data;
+	}
+
+	const TYPE& operator[] (unsigned int index) const
+	{
+		assert(index < size);
+		return getNodeAtPos(index)->data;
 	}
 
 	doubleNode<TYPE>* getFirst() const
@@ -194,6 +323,5 @@ public:
 	}
 
 };
-
 
 #endif // !__DLIST_H__
--- DynArray.h
+++ DynArray.h
@@ -2,6 +2,7 @@
 #define __DYNARRAY_H__
 
 #include <assert.h> 
+#include "DList.h"
 
 #define DYN_ARRAY_BLOCK_SIZE 16
 
@@ -16,44 +17,20 @@ private:
 
 	void reallocate(unsigned int new_mem_size)
 	{
-		TYPE *tmp = data;
-
 		allocated_memory = new_mem_size;
-		data = new TYPE[allocated_memory];
+		TYPE *tmp = new TYPE[allocated_memory];
 
-		if (num_elements > allocated_memory)
-			num_elements = allocated_memory;
+	/*	if (num_elements > allocated_memory)
+			num_elements = allocated_memory;*/
 
-		if (tmp != NULL)
+		if (data != NULL)
 		{
 			for (unsigned int i = 0; i < num_elements; i++)
-				data[i] = tmp[i];
-
-			delete[] tmp;
-		}
-		
-		// CRZ code
-		/*if (data != NULL)
-		{
-			TYPE *tmp = new TYPE[allocated_memory];
-			for (unsigned int i = 0; i < num_elements; i++)
-			{
 				tmp[i] = data[i];
-			}
+
 			delete[] data;
-			allocated_memory = new_mem_size;
-			data = new TYPE[allocated_memory];
-			for (unsigned int i = 0; i < num_elements; i++)
-			{
-				data[i] = tmp[i];
-			}
 		}
-		else
-		{
-			delete[] data;
-			allocated_memory = new_mem_size;
-			data = new TYPE[allocated_memory];
-		}*/
+		data = tmp;
 	}
 
 public:
@@ -76,10 +53,20 @@ public:
 			pushBack(array.data[i]);
 	}
 
-
 	~DynArray<TYPE>()
 	{
 		delete[] data;
+	}
+
+	const DynArray &operator+= (const DynArray &array_to_cat)
+	{
+		if (getNumElements() + array_to_cat.getNumElements() > allocated_memory)
+			reallocate(getNumElements() + array_to_cat.getNumElements());
+
+		for (unsigned int i = 0; i < array_to_cat.getNumElements(); i++)
+			pushBack(array_to_cat[i]);
+
+		return (*this);
 	}
 
 	void pushBack(TYPE new_value)
@@ -96,7 +83,7 @@ public:
 		// When the element is deleted, it is necessary to return a copy of that element.
 		if (data != NULL && num_elements > 0)
 		{
-			value = data[--num_elements];					
+			value = data[--num_elements];
 			return true;
 		}
 		return false;
@@ -111,10 +98,10 @@ public:
 			{
 				tmp[i] = data[i];
 			}
-			
+
 			if (num_elements == allocated_memory)
 				reallocate(allocated_memory + 1);
-			
+
 			for (unsigned int i = 0; i < position; i++)
 			{
 				data[i] = tmp[i];
@@ -131,6 +118,41 @@ public:
 		return false;
 	}
 
+	bool insert(const DynArray &array_inserted, unsigned int position)
+	{
+		if (position <= num_elements)
+		{ 
+			unsigned int num_elements_inserted = array_inserted.getNumElements();
+			TYPE *tmp = new TYPE[num_elements + num_elements_inserted];
+
+			if (num_elements + num_elements_inserted > allocated_memory)
+				reallocate(num_elements + num_elements_inserted);
+			
+			for (unsigned int i = position; i < num_elements; i++)
+				tmp[num_elements_inserted + i] = data[i];
+
+			for (unsigned int i = 0; i < num_elements_inserted; i++)
+			{
+				data[position + i] = array_inserted[i];
+				num_elements++;
+			}
+
+			for (unsigned int i = position + num_elements_inserted; i < num_elements; i++)
+				data[i] = tmp[i];
+
+			delete[] tmp;
+			return true;
+		}
+		return false;
+	}
+
+	void flip()
+	{
+		unsigned int loops = num_elements / 2;
+		for (unsigned int i = 0; i < loops; i++)
+			swap(data[i], data[num_elements - (1 + i)]);
+	}
+
 	bool clear()
 	{
 		if (data != NULL)
@@ -141,29 +163,169 @@ public:
 		return false;
 	}
 
+	unsigned int bubble_sort()
+	{
+		// BUBBLE SORT
+		unsigned int changes;
+		unsigned int counter = 0;
+		do
+		{
+			changes = 0;
+			for (unsigned int i = 0; i < num_elements - 1; i++)
+			{
+				counter++;
+				if (data[i] > data[i + 1])
+				{
+					swap(data[i], data[i + 1]);
+					changes++;
+				}
+			}
+		} while (changes != 0);
+
+		return counter;
+	}
+
+	unsigned int bubble_sort2()
+	{
+		// BUBBLE SORT OPTIMIZED
+		// Number of iterations is reduced by 2. Each pair of values 
+		// are compared only once.
+
+		unsigned int counter = 0;
+
+		for (unsigned int i = 0; i < num_elements - 1; i++)
+		{
+			for (unsigned int j = i + 1; j < num_elements; j++)
+			{
+				counter++;
+				if (data[i] > data[j])
+				{
+					swap(data[i], data[j]);
+				}
+			}
+		}
+
+		return counter;
+	}
+
+	unsigned int sort_super_opt()
+	{
+		// BUBBLE SORT SUPER OPTIMIZED
+		// Otra forma de optimizarlo es saber donde se produce el último swap. 
+		// Hasta ese punto, todo estará ordenado a la derecha.
+
+		// Otra forma es recorrer la array en dos sentidos a cada paso por la array.
+
+		// Comsort?  Existeix un GAP que es la diferencia que hi ha entre dos números que
+		// es comproven. Al inici és el 70% de l'array. Després va baixant.
+
+		unsigned int counter = 0;
+		unsigned int last = num_elements;
+
+		for (unsigned int i = 0; i < last - 1; i++)
+		{
+			for (unsigned int j = i + 1; j < last; j++)
+			{
+				counter++;
+				if (data[i] > data[j])
+				{
+					swap(data[i], data[j]);
+					last = j;
+				}
+			}
+		}
+
+		return counter;
+	}
+
+	unsigned int combsort()
+	{
+		unsigned int counter = 0;
+
+		float shrink_factor = 1.3f;
+		unsigned int gap = (int)(num_elements / shrink_factor);
+		bool swapped = true;
+
+	    while (swapped == true)
+		{			
+			swapped = false;
+
+			if (gap < 1)
+				gap = 1;
+			
+			for (unsigned int i = 0; i + gap < num_elements; i++)
+			{
+				counter++;
+				if (data[i] > data[i + gap])
+				{
+					swap(data[i], data[i + gap]);
+					swapped = true;
+				}
+			}
+			gap = (int)(gap / shrink_factor);
+		}
+		return counter;
+	}
+
+	unsigned int cocktailSort()
+	{
+		unsigned int counter = 0; 
+		int begin = -1;
+		int end = num_elements - 1;
+		bool swapped = true;
+
+		while (swapped)
+		{
+			swapped = false;
+			begin++;
+			for (int i = begin; i < end; i++)
+			{
+				counter++;
+				if (data[i] > data[i + 1])
+				{
+					swap(data[i], data[i + 1]);
+					swapped = true;
+				}
+			}
+
+			end--;
+			for (int i = end; i >= begin; i--)
+			{
+				counter++;
+				if (data[i] > data[i + 1])
+				{
+					swap(data[i], data[i + 1]);
+					swapped = true;
+				}
+			}
+		}
+
+		return counter;
+	}
+
 	// Auxiliar method useful to represent all the information.
 	// It won't have its correspoding Unittest test. 
 	void info() const
 	{
-		for (int i = 0; i < num_elements; i++)
+		for (unsigned int i = 0; i < num_elements; i++)
 		{
 			printf("%s %d => %d\n", "Position", i, data[i]);
 		}
 		printf("%s: %d\n", "Number of elements", num_elements);
 		printf("%s: %d\n\n", "Allocated memory", allocated_memory);
 	}
-	
+
 	TYPE& operator[] (unsigned int index)
 	{
 		// For p[1] = 15;
-		assert (index < num_elements);
+		assert(index < num_elements);
 		return data[index];
 	}
-		
+
 	const TYPE& operator[] (unsigned int index) const
 	{
 		// For printf("%d", p[1]);
-		assert (index < num_elements);
+		assert(index < num_elements);
 		return data[index];
 	}
 
@@ -177,6 +339,5 @@ public:
 		return num_elements;
 	}
 };
-
 
 #endif // !__DYNARRAY_H__
